c2983b105326e68ab8e53ae7ba062ede09af1541
Mohammed <mohammedredha908@gmail.com>
2024-05-25 22:21:47 +0300

    lualine - bubble theme

--- lua/mohammed/plugins/lualine.lua
+++ lua/mohammed/plugins/lualine.lua
@@ -1,24 +1,14 @@
 return {
-  "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    local lualine = require("lualine")
-    local lazy_status = require("lazy.status") -- to configure lazy pending updates count
-    lualine.setup({
-      options = {
-        theme = "auto",
-      },
-      sections = {
-        lualine_x = {
-          {
-            lazy_status.updates,
-            cond = lazy_status.has_updates,
-          },
-          { "encoding" },
-          { "fileformat" },
-          { "filetype" },
-        },
-      },
-    })
-  end,
+	"nvim-lualine/lualine.nvim",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		local lualine = require("lualine")
+		lualine.setup({
+			options = {
+				theme = "auto",
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+		})
+	end,
 }
