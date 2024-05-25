064bb36e5547b495196233f276383c1e48133d25
Mohammed <mohammedredha908@gmail.com>
2024-05-25 16:54:46 +0300

    fixed kemaps conflict

--- init.lua
+++ init.lua
@@ -1,6 +1,6 @@
-require("mohammed.core")
 require("mohammed.lazy")
 vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
 vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
 vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
 vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
+require("mohammed.core") -- don't change the order of this line, so it overrides other keymaps
--- lua/mohammed/core/keymaps.lua
+++ lua/mohammed/core/keymaps.lua
@@ -1,47 +1,42 @@
 vim.g.mapleader = " "
 
 local keymap = vim.keymap -- for conciseness
-
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
-
-local keymap = vim.keymap -- for conciseness
-
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set({ "v", "i" }, "jk", "<ESC>", { desc = "Go to normal mode" })
 
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
+keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 
 -- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
 
 -- Custom motions
-keymap.set({ "n", "v" }, "<C-j>", "G", { desc = "Go to the end of the file" })
-keymap.set("n", "y<C-j>", "yG", { desc = "Copy to the end of the file" })
-keymap.set("n", "d<C-j>", "dG", { desc = "Delete to the end of the file" })
+keymap.set({ "n", "v" }, "<C-j>", "G", { desc = "Go to the end of the file", noremap = true, silent = true })
+keymap.set("n", "y<C-j>", "yG", { desc = "Copy to the end of the file", noremap = true, silent = true })
+keymap.set("n", "d<C-j>", "dG", { desc = "Delete to the end of the file", noremap = true, silent = true })
 
-keymap.set({ "n", "v" }, "<C-k>", "gg", { desc = "Go to the top of the file" })
-keymap.set("n", "y<C-k>", "ygg", { desc = "Copy to the top of the file" })
-keymap.set("n", "d<C-k>", "dgg", { desc = "Delete to the top of the file" })
+keymap.set({ "n", "v" }, "<C-k>", "gg", { desc = "Go to the top of the file", noremap = true, silent = true })
+keymap.set("n", "y<C-k>", "ygg", { desc = "Copy to the top of the file", noremap = true, silent = true })
+keymap.set("n", "d<C-k>", "dgg", { desc = "Delete to the top of the file", noremap = true, silent = true })
 
-keymap.set({ "n", "v" }, "<C-l>", "$", { desc = "Go to the end of the current line" })
-keymap.set("n", "y<C-l>", "y$", { desc = "Copy to the end of the current line" })
-keymap.set("n", "d<C-l>", "d$", { desc = "Delete to the end of the current line" })
+keymap.set({ "n", "v" }, "<C-l>", "$", { desc = "Go to the end of the current line", noremap = true, silent = true })
+keymap.set("n", "y<C-l>", "y$", { desc = "Copy to the end of the current line", noremap = true, silent = true })
+keymap.set("n", "d<C-l>", "d$", { desc = "Delete to the end of the current line", noremap = true, silent = true })
 
-keymap.set({ "n", "v" }, "<C-h>", "0", { desc = "Go to the start of the current line" })
-keymap.set("n", "y<C-h>", "y0", { desc = "Copy to the start of the current line" })
-keymap.set("n", "d<C-h>", "d0", { desc = "Delete to the start of the current line" })
+keymap.set({ "n", "v" }, "<C-h>", "0", { desc = "Go to the start of the current line", noremap = true, silent = true })
+keymap.set("n", "y<C-h>", "y0", { desc = "Copy to the start of the current line", noremap = true, silent = true })
+keymap.set("n", "d<C-h>", "d0", { desc = "Delete to the start of the current line", noremap = true, silent = true })
 
 keymap.set({ "n", "v" }, "<C-d>", "<C-d>zz") -- center the cursor automatically
 keymap.set({ "n", "v" }, "<C-u>", "<C-u>zz") -- center the cursor automatically
