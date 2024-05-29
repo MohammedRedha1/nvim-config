83a98e0a2817f0ae1110390016e3417d37966bdd
Mohammed <mohammedredha908@gmail.com>
2024-05-29 14:47:48 +0300

    added color highlight

--- lua/mohammed/plugins/harpoon.lua
+++ lua/mohammed/plugins/harpoon.lua
@@ -9,8 +9,13 @@ return {
 			':lua require("harpoon.ui").toggle_quick_menu()<CR>',
 			{ desc = "toggle harpoon menu" }
 		)
-		keymap.set("n", "<leader>ha", ':lua require("harpoon.mark").add_file()<CR>', { desc = "harpoon - add a file" })
-		keymap.set("n", "<leader>j", ':lua require("harpoon.ui").nav_next()<CR>', { desc = "harpoon - next" })
-		keymap.set("n", "<leader>k", ':lua require("harpoon.ui").nav_prev()<CR>', { desc = "harpoon - previous" })
+		keymap.set(
+			"n",
+			"<leader>ha",
+			':lua require("harpoon.mark").add_file()<CR>',
+			{ desc = "harpoon - add current file" }
+		)
+		keymap.set("n", "<A-j>", ':lua require("harpoon.ui").nav_next()<CR>', { desc = "harpoon - next" })
+		keymap.set("n", "<A-k>", ':lua require("harpoon.ui").nav_prev()<CR>', { desc = "harpoon - previous" })
 	end,
 }
